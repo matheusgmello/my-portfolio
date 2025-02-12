9b12144bdab5f3fb4ecde908eef15634028e6095
Matheus Mello <mathgfmello@gmail.com>
2025-02-12 14:22:48 -0300

    fix: download do pdf

--- src/components/DownloadButton/DownloadButton.jsx
+++ src/components/DownloadButton/DownloadButton.jsx
@@ -4,7 +4,7 @@ import resume from "../../assets/resume.pdf";
 
 const DownloadButton = (props) => {
   return (
-    <a href={resume} download="Matheus_Mello.pdf">
+    <a href={resume} download="cv-matheus-mello.pdf">
     <div onClick={props.onClick} className="download-btn">
       <span className="download-txt">Download CV</span>
     </div>
